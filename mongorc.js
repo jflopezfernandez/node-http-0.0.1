ab75928ae7b10ec5c110eca17d5d1ab100e6a744
Jose Fernando Lopez Fernandez <jflopezfernandez@gmail.com>
2018-02-17 18:39:37 -0500

    Initial commit

---
+++ mongorc.js
@@ -0,0 +1,7 @@
+
+// const compliment = [ "attractive", "intelligent", "like Batman" ];
+// const index = Math.floor(Math.random() * 3);
+//
+// console.log("Hello, you're looking particularly " + compliment[index] + " today!");
+
+mongo --nodb
